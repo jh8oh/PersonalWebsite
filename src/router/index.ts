9abf6e37e29d954e5a03160702bf673a621d743e
Ji Ho Oh <ohjiho.ca@gmail.com>
2021-08-18 02:12:42 -0400

    Added scroll to top with delay upon transition

--- src/router/index.ts
+++ src/router/index.ts
@@ -23,6 +23,13 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+  scrollBehavior() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ x: 0, y: 0 })
+      }, 400)
+    });
+  },
 });
 
 export default router;
